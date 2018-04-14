793a8bde9ae1fe93b4e1c5bed7a041ec7acb53d4
Hampus Lundqvist <haslun16@edu.vilhelmina.se>
2018-04-14 23:01:35 +0200

    fixed wm

--- src/reducers/windowManager.js
+++ src/reducers/windowManager.js
@@ -5,9 +5,9 @@ const windowManager = (state = initState, action) => {
   if (action.reducer !== 'WM') { return state }
   switch (action.type) {
     case 'CREATE':
-      return { windows: createWindow(state.windows, action) }
+      return { windows: createWindow({...state.windows}, action) }
     case 'DISCARD':
-      return { windows: discardWindow(state.windows, action) }
+      return { windows: discardWindow({...state.windows}, action) }
     default:
       return state
   }
@@ -17,8 +17,8 @@ const createWindow = (windows, action) => {
   const id = Math.random().toString(36).substr(2, 5)
   const newWindow = {
     windowID: id,
-    x: 32 + (28 * Object.keys(windows).length),
-    y: 32 + (28 * Object.keys(windows).length),
+    x: 32 + (16 * Object.keys(windows).length),
+    y: 32 + (16 * Object.keys(windows).length),
     height: 200,
     width: 300,
     appPath: action.appPath
